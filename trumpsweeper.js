e828e590c991283f9fd83eaf3de0522de6be1a98
Alexis Moody <amoodyldes@gmail.com>
2016-10-15 23:00:24 -0400

    complete game board generation logic

--- trumpsweeper.js
+++ trumpsweeper.js
@@ -22,41 +22,77 @@ var Game = function(){
 		var mineLocations = [];
 		var row = 0;
 		var cell = 0;
+		function isThereADup(array, object){
+			for(var i = 0; i < array.length - 1; i++){
+				if(array[i].row == object.row && array[i].cell == object.cell){
+					return true;
+				}
+			}
+			return false;
+		}
+		function generateLocation(){
+			row = Math.floor(Math.random() * (9 - 0)) + 0;
+			cell = Math.floor(Math.random() * (9 - 0)) + 0;
 
+			return {row: row,cell: cell};
+		}
 		for(var i = 0; i < 9; i++){
-			row = Math.random() * (9 - 0) + 0;
-			cell = Math.random() * (9 - 0) + 0;
-			mineLocations.push({row: row,cell: cell});
+			var location = generateLocation();
+			var check = isThereADup(mineLocations, location);
+
+			while(check){
+				location = generateLocation();
+				check = isThereADup(mineLocations, location);
+			}
+
+			mineLocations.push(location);
 		}
 
-		mineLocations.sort(function(a,b){
-			if(a.row > b.row){
-				return 1;
-			}
-			if(a.row < b.row){
-				return -1;
-			}
-			return 0;
-		})
+	
 		console.log(mineLocations);
 		return mineLocations;
 	}
 	this.generateBoard = function(){
-		var generatedMines = this.generateMines;
+		var generatedMines = this.generateMines();
+
 		for(var i = 0; i < 9; i++){
 			var currentRow = "row" + i;
 			for(var x = 0; x < 9; x++){
 				this.board[currentRow].push(new Cell([i,x]));
 			}
 		}
+		var x;
+		for(x in generatedMines){
+			var row = "row" + generatedMines[x].row;
+			var cell = generatedMines[x].cell;
+			this.board[row][cell].isMine = true;
+		}
+	}
+	this.printBoard = function(){
+		for(var x = 0; x < 9; x++){
+			var row = "row" + x;
+			for(var y = 0; y < 9; y++){
+				if(this.board[row][y].isMine == true){
+					$("#row-"+x+">#cell-"+y).text("X");
+				}
+			}
+		}
+	}
+	this.clearBoard = function(){
+		for(var x = 0; x < 9; x++){
+			for(var y = 0; y < 9; y++){
+				$("#row-"+x+">#cell-"+y).text("");
+			}
+		}
 	}
 };
 
 $(document).ready(function(){
-	console.log("hello");
 	$("button").on("click", function(){
-		// var newGame = new Game();
-		// newGame.generateBoard();
-		console.log("newGame");
+		var newGame = new Game();
+		newGame.clearBoard();
+		newGame.generateBoard();
+		newGame.printBoard();
+		
 	})
 });
